28aace376d3e2dea59597ca2948f0f6197db23e3
marcello coelho <marcellogcoelho@outlook.com>
2024-10-01 18:08:17 -0300

    feat: router outlet on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { FibonacciComponent } from "./components/fibonacci/fibonacci.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, FibonacciComponent],
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
-  title = 'target';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
+import { FibonacciComponent } from "./components/fibonacci/fibonacci.component";
+import { LayoutComponent } from "./components/layout/layout.component";
+import { NgModule } from "@angular/core";
+import { CharacterLocationComponent } from "./components/character-location/character-location.component";
+import { LoopComponent } from "./components/loop/loop.component";
+import { SequencesComponent } from "./components/sequences/sequences.component";
+import { LightbulbsComponent } from "./components/lightbulbs/lightbulbs.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: LayoutComponent,
+    children: [
+      { path: "fibonacci", component: FibonacciComponent },
+      { path: "verificar", component: CharacterLocationComponent },
+      { path: "loop", component: LoopComponent },
+      { path: "sequencias", component: SequencesComponent },
+      { path: "lampadas", component: LightbulbsComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
