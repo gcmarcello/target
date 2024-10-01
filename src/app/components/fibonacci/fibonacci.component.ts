73afaffc3820dca6757fece13cd99b6ca9e14d67
marcello coelho <marcellogcoelho@outlook.com>
2024-10-01 15:06:52 -0300

    feat: fibonacci component

---
+++ src/app/components/fibonacci/fibonacci.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fibonacci',
+  standalone: true,
+  imports: [],
+  templateUrl: './fibonacci.component.html',
+  styleUrl: './fibonacci.component.scss'
+})
+export class FibonacciComponent {
+
+}
